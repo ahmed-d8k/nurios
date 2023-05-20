4ec95f6dad889cbab9cfb2e6d10d19023cc3656c
ncn-ends <ncn.ends@gmail.com>
2023-05-20 04:45:21 -0700

    style changes

---
+++ client/src/components/AppCanvas.tsx
@@ -0,0 +1,158 @@
+import {createSignal, onMount} from "solid-js";
+
+const canvasId = "main-canvas";
+
+const canvasWidth = 300;
+const canvasHeight = 300;
+type BoxDrawing = {startX: number, startY: number, width: number, height: number};
+const getUsefulDataFromEvent = (e: MouseEvent) => ({
+    mouseX: e.offsetX,
+    mouseY: e.offsetY
+})
+
+const [firstPoint, setFirstPoint] = createSignal<[number, number] | null>(null)
+const firstPointExists = () => firstPoint() !== null;
+const [boxes, setBoxes] = createSignal<BoxDrawing[]>([]);
+
+const [canvasCtx, setCanvasCtx] = createSignal(null);
+const [canvasRect, setCanvasRect] = createSignal(null);
+
+const setStrokeStyle = (color: string) => {
+    const ctx = canvasCtx();
+    if (!ctx) return;
+    ctx.strokeStyle = color;
+}
+const setDrawStyle = () => setStrokeStyle("#000000");
+const setDrawingStyle = () => setStrokeStyle("#FF0800");
+const setUpDrawing = () => {
+    const canvas = document.getElementById<HTMLCanvasElement>(canvasId);
+    const rect = canvas.getBoundingClientRect();
+    const ctx = canvas?.getContext("2d");
+    setCanvasCtx(ctx);
+    setCanvasRect(rect);
+    // canvas.style.cursor = "crosshair"y
+
+    if (canvas?.getContext) {
+        ctx.fillStyle = "rgb(200, 0, 0)";
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect(30, 30, 50, 50);
+
+        ctx.strokeRect(45, 45, 60, 60);
+    }
+
+    const handleMouseDown = (e: MouseEvent) => {
+        if (e.target.id !== canvasId) return;
+        const {mouseX, mouseY} = getUsefulDataFromEvent(e);
+
+        setFirstPoint([mouseX, mouseY]);
+        canvas.style.cursor = "crosshair";
+    }
+
+    const handleMouseUp = (e: MouseEvent) => {
+        if (e.target.id !== canvasId) return;
+
+        /* this means the user started the click outside the canvas */
+        if (!firstPointExists()) return;
+
+        const {mouseX, mouseY} = getUsefulDataFromEvent(e);
+        const [startX, startY] = firstPoint();
+
+        const diffX = Math.abs(mouseX - startX);
+        const diffY = Math.abs(mouseY - startY);
+
+        if (diffX <= 10 || diffY <= 10) return;
+
+        canvas.style.cursor = "default";
+        setBoxes(p => [...p, {
+            startX,
+            startY,
+            width: mouseX - startX,
+            height: mouseY - startY
+        }]);
+        redrawAllBoxes();
+
+        return setFirstPoint(null);
+    }
+
+    const handleMouseMove = (e: MouseEvent) => {
+        if (e.target.id != canvasId) return;
+        if (!firstPointExists()) return;
+
+        redrawAllBoxes();
+
+        const {mouseX, mouseY} = getUsefulDataFromEvent(e);
+        const [startX, startY] = firstPoint();
+
+        /* draw a dashed line with set color while dragging, then reset back to original rectangle style */
+        ctx.save();
+        ctx.beginPath();
+        setDrawingStyle();
+        ctx.setLineDash([6]);
+        ctx.strokeRect(startX, startY, mouseX - startX, mouseY - startY);
+        ctx.restore();
+    }
+
+    const handleKeyPress = (e: KeyboardEvent) => {
+        if (e.code === "Escape") {
+            setFirstPoint(null);
+            redrawAllBoxes();
+            canvas.style.cursor = "default";
+        }
+    }
+
+    canvas.addEventListener("mousemove", handleMouseMove, false);
+    canvas.addEventListener("mousedown", handleMouseDown, false);
+    canvas.addEventListener("mouseup", handleMouseUp, false);
+
+    /* keypress doesn't fire when escape is pressed */
+    window.addEventListener("keydown", handleKeyPress, false);
+}
+
+/* instead of drawing rectangles on top of the canvas, we redraw after every submitted action
+* this allows us to do things like undo, show drawing rectangle, change color, change dimensions, etc. more easily by just manipulating state
+* */
+const redrawAllBoxes = () => {
+    const ctx = canvasCtx();
+    const rect = canvasRect();
+    if (!ctx || !rect) return;
+
+    ctx.clearRect(0, 0, rect.width, rect.height);
+    boxes().forEach(box => {
+        ctx.beginPath();
+        ctx.rect(box.startX, box.startY, box.width, box.height);
+        setDrawStyle();
+        ctx.stroke();
+    })
+}
+
+const handleResetButton = () => {
+    if (!canvasCtx()) return;
+
+    canvasCtx().reset();
+    setBoxes([]);
+}
+
+const handleUndoButton = () => {
+    if (!canvasCtx()) return;
+
+    setBoxes(p => p.slice(0, p.length - 1))
+    redrawAllBoxes();
+}
+
+export const AppCanvas = () => {
+    onMount(() => setUpDrawing());
+
+    return (
+        <>
+            <canvas id={canvasId} width={canvasWidth} height={canvasHeight} class={"bg-red-400 relative"} >
+                Image canvas not loaded
+            </canvas>
+            <div class={"my-4 gap-4 flex"}>
+                <button class="text-gray-300 hover:text-sky-600" onclick={handleResetButton}>Reset</button>
+                <button class="text-gray-300 hover:text-sky-600" onclick={handleUndoButton}>Undo</button>
+            </div>
+        </>
+    )
+}
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,32 +1,31 @@
-import { A } from "solid-start";
+import {A} from "solid-start";
 import Counter from "~/components/Counter";
-import {Canvas} from "~/components/Canvas";
+import {AppCanvas} from "~/components/AppCanvas";
 
 export default function Home() {
-  return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
-      </h1>
-      <Canvas />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
-    </main>
-  );
+    return (
+        <main class="text-center flex flex-col items-center text-gray-700 p-4 bg-neutral-800">
+            <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-4">
+                Capstone
+            </h1>
+            <p class={"text-md text-neutral-500"}>
+                Tips
+            </p>
+            <ul class={"mb-8"}>
+                <li class={"text-sm text-neutral-500"}>- Press ctrl + Z to undo last box</li>
+                <li class={"text-sm text-neutral-500"}>- Press Esc to stop drawing</li>
+            </ul>
+            <AppCanvas/>
+            <p class="my-4 flex gap-2">
+                <span>Home</span>
+                <span>{" - "}</span>
+                <A href="/about" class="text-sky-600 hover:underline">
+                    About Page
+                </A>
+                <span>{" - "}</span>
+                <img width="25" height="25"
+                     src={"https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg"}/>
+            </p>
+        </main>
+    );
 }
