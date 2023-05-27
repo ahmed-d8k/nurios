c64b1b23d1397da208b0d6cf81fde2f607420684
ncn-ends <ncn.ends@gmail.com>
2023-05-27 16:50:56 -0700

    minimal example with websocket

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 
 class Item(BaseModel):
@@ -7,13 +7,23 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
-
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/ping")
+async def root():
+    return "pong"
+
 @app.post("/process")
 async def process(item: Item):
     return item
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
