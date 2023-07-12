b15a4f231387b225264c03ed6b29dde9039dd6ba
ncn-ends <ncn.ends@gmail.com>
2023-07-11 19:02:09 -0700

    generic error handle and healthy response handle

--- client/src/shared/error-state.ts
+++ client/src/shared/error-state.ts
@@ -1,11 +1,13 @@
 import {createSignal} from "solid-js";
 
 export enum ErrorHumanMessageEnum {
-  BadInput = "Server received invalid input, but this is unexpected. Please open an issue on GitHub. You can try to refresh the page and try again. "
+  BadInput = "Server received invalid input, but this is unexpected. You can try to refresh the page and try again. ",
+  UncaughtError = "An unexpected unknown error occured. "
 }
 
 export interface ErrorData {
   msg: ErrorHumanMessageEnum;
   additionalDataStr?: string;
 }
+
 export const [lastError, setLastError] = createSignal<ErrorData | null>(null);
--- client/src/shared/resources.ts
+++ client/src/shared/resources.ts
@@ -17,25 +17,30 @@ interface SAMInputModel {
   boxes: SAMInputModelBox[]
 }
 
-export const submit = async (model: SAMInputModel) => {
-  return wretch("http://localhost:8080/process")
-    .post()
-    .error(422, err => {
-      setLastError({
-        msg: ErrorHumanMessageEnum.BadInput,
-        additionalDataStr: err.response.body ? JSON.stringify(err.message) : undefined
-      })
-    })
-    .res(res => res.body);
-  // try {
-  //   const qwe = await axios.post<SubmitResponse>(`http://localhost:8080/process`);
-  //   console.log(qwe.data);
-  //   return qwe;
-  // } catch (e: AxiosError<Error>) {
-  //   e.toJSON()
-  //   setLastError({msg: ErrorHumanMessageEnum.std})
-  //
-  //   // console.log(e.toJSON());
-  //   // return e.toJSON();
-  // }
+enum EndpointEnum {
+  Process = "process"
 }
+
+const baseUrl = "http://localhost:8080"
+
+export const submit = async (model: SAMInputModel) =>
+  wretch(`${baseUrl}/${EndpointEnum.Process}`)
+    .post(model)
+    .error(422, err => setLastError({
+      msg: ErrorHumanMessageEnum.BadInput,
+      additionalDataStr: err.response.body ? JSON.stringify(err.message) : undefined
+    }))
+    // .res(res => {
+    //   console.log(res);
+    //   console.log("body", res.body.);
+    //   return res.body
+    // })
+    .json(res => {
+      console.log(res);
+      console.log("body", res);
+      return res;
+    })
+    .catch(err => setLastError({
+      msg: ErrorHumanMessageEnum.UncaughtError,
+      additionalDataStr: err.response.body ? JSON.stringify(err.message) : undefined
+    }))
