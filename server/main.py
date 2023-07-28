e2e09b4f8ff88dd626254e8b8c263bdbac3e4eba
ahmed-d8k <68250969+ahmed-d8k@users.noreply.github.com>
2023-07-27 17:24:23 -0700

    Update main.py
    reverting changes since appraoch breaks program

--- server/main.py
+++ server/main.py
@@ -131,8 +131,7 @@ class BoxData(BaseModel):
 # async def upload_file(file: UploadFile,
 #                       model: Base = Depends(checker)):
 
-# Temp important while troubleshooting
-import time
+
 
 async def upload_file(file: UploadFile = File(...),
                       intro: str = Form(...),
@@ -172,8 +171,6 @@ async def upload_file(file: UploadFile = File(...),
 
     while sam.is_in_use():
         pass
-    else:
-        time.sleep(5)
     seg_image, outline_image = sam.process(transformed_boxes, input_image)
     sam.set_not_in_use()
 
